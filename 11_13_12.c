84bec00f6582fa3f171c21c41489528443dc1c31
wancaibida <wancaibida@gmail.com>
2017-03-13 00:07:22 +0800

    chapter 11

--- 11_13_12.c
+++ 11_13_12.c
@@ -1,25 +1,79 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 #define MAX_LEN 20
-#define MAX_WORD 5
+#define MAX_WORD 10
 
 char* read_line(char line[], int len);
+int read_word(char * line, int x, int y, char arr[x][y]);
 
 int main(void)
 {
 	char line[MAX_LEN];
-	char * words[5];
+	char words[5][10];
+	int read, i;
 
 	read_line(line, MAX_LEN);
-	puts(line);
+	read = read_word(line, 5, 10, words );
+
+	for (i = read; i > -1; --i)
+	{
+		printf("%s ", words[i]);
+	}
 
 	return 0;
 }
 
+int read_word(char * line, int x, int y, char arr[x][y])
+{
+	int i, w;
+	size_t j;
+	char *ptr;
+	size_t len = strlen(line);
+	w = 0;
+
+	for (i = 0; i < len; )
+	{
+		ptr = line + i;
+		j = 0;
+
+		if (*ptr == '\0')
+		{
+			return w;
+		}
+
+		while (isalpha(*ptr))
+		{
+			++ptr;
+			++j;
+		}
+
+		if (j)
+		{
+			if (w < y)
+			{
+				strncpy(arr[w], line + i, j);
+				arr[w][j] = '\0';
+				++w;
+				i += j;
+			}
+			else
+			{
+				return w;
+			}
+		}
+		else
+		{
+			++i;
+		}
+	}
+
+	return w;
+}
+
 char* read_line(char line[], int len)
 {
-	int i = 0;
-
 	fgets(line, len, stdin);
 
 	size_t ln = strlen(line) - 1;
